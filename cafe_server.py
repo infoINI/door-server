f3563679363101a6a822d451fc0e018c2fa8fc40
Herman Fries <baracoder@googlemail.com>
2016-10-25 00:50:18 +0200

    add prometheus instrumentation

--- cafe_server.py
+++ cafe_server.py
@@ -4,17 +4,18 @@ import serial
 import SocketServer
 import threading
 import json
-from serial.serialutil import SerialTimeoutException, SerialException
 
+from prometheus_client import Gauge, Counter, start_http_server
+s = Gauge('ini_door_state', 'Ini door open-1, closed-0')
+c = Counter('ini_door_requests', 'Ini door requests')
 
-#######################################################################
-## TCP Kommunikation
 class TCPRequestHandler(SocketServer.BaseRequestHandler):
     def __init__(self, callback, *args, **keys):
         self.callback = callback
         SocketServer.BaseRequestHandler.__init__(self, *args, **keys)
 
     def handle(self):
+        c.inc()
         data = self.callback()
         print "request: " + data
         self.request.send(data)
@@ -31,16 +32,19 @@ class TuerSensor(object):
         self._s.setRTS()
 
     def ist_offen(self):
-        return not self._s.getCTS()
+        d_open = not self._s.getCTS()
+        s.set( 1 if d_open else 0 )
+        return d_open
 
 
 
-
-#######################################################################
-## Server
 
 class CafeServer(object):
-    def _startTCP(self):
+    def __init__(self):
+        port_tuer    = '/dev/ttyS0'
+
+        self._tuer = TuerSensor(port=port_tuer)
+
         ## TCP Server starten
         # port CAFE, 51966
         HOST, PORT = "0.0.0.0", 0xCAFE
@@ -49,16 +53,9 @@ class CafeServer(object):
                 cafeserver.getJson, *args, **keys))
         self.server_thread = threading.Thread(target=self.server.serve_forever)
         self.server_thread.daemon = True
-        self.server_thread.start()
-
-    def __init__(self):
-        port_tuer    = '/dev/ttyS0'
-
-        self._tuer = TuerSensor(port=port_tuer)
-
-        self._startTCP()
 
     def run(self):
+        self.server_thread.start()
         self.server_thread.join()
 
     def getData(self):
@@ -76,6 +73,7 @@ class CafeServer(object):
 #######################################################################
 ## Start
 if __name__ == "__main__":
+    start_http_server(0xCAFF)
     try:
         cafeserver = CafeServer()
         cafeserver.run()
